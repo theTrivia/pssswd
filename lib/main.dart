458055fac33628a3b99db3a4dc49b4b346a726c1
theTrivia <bfmvsp@gmail.com>
2022-06-07 15:58:32 +0530

    removing leading # from urls using url_strategy package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import './providers/user_entries.dart';
 import './screens/landingPage.dart';
@@ -16,7 +17,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  setPathUrlStrategy();
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
